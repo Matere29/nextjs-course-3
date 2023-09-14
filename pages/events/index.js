762e136c47360c1e8f1f5e86b3e4b82c921a2585
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-09-14 16:17:34 +0200

    Add files via upload

---
+++ pages/events/index.js
@@ -0,0 +1,34 @@
+import { getAllEvents } from '../helpers/api-util'
+import EventList from '../../components/events/event-list'
+import EventSearch from '../../components/events/events-search'
+import { Fragment, ReactFragment } from 'react'
+import { useRouter } from 'next/router'
+
+export default function AllEventsPage(props) {
+  const router = getAllEvents()
+ const {events} = props
+
+  function findEventsHandler(year, month){
+const fullPath = `/events/${year}/${month}`
+router.push(fullPath);
+  }
+  return (
+
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+
+    </Fragment>
+  )
+}
+export async function getStaticProps(){
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  }
+
+}
